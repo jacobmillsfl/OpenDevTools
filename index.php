4ff187f1a1104fe8309aab3061c75f1eb92c6cd8
H0r53 <jacobmillsfl@gmail.com>
2017-10-11 21:18:54 -0400

    Updated image for DALGen and TaskTracker

--- index.php
+++ index.php
@@ -181,7 +181,7 @@ include("DAL/SiteBanner.php");
         </div>
         <div class="col-lg-4 col-sm-6 portfolio-item">
             <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <a href="#"><img class="card-img-top" src="images/dalgenlogo.png" alt=""></a>
                 <div class="card-body">
                     <h4 class="card-title">
                         <a href="#">DALGen</a>
@@ -192,7 +192,7 @@ include("DAL/SiteBanner.php");
         </div>
         <div class="col-lg-4 col-sm-6 portfolio-item">
             <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <a href="#"><img class="card-img-top" src="images/tasktrackerlogo.png" alt=""></a>
                 <div class="card-body">
                     <h4 class="card-title">
                         <a href="#">TaskTracker</a>
