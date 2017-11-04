fbdd4a0d11932ff38455324a6ea50ab9f6a78fd0
carlaoutput <carla.output@gmail.com>
2017-11-04 16:36:22 -0400

    Enter Donation botton to paypal

--- footer.php
+++ footer.php
@@ -9,8 +9,32 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
     <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; opendevtools.org 2017 - <a href="/policy">Private
-                Policy</a></p>
+        <div class="">
+            <p class="m-0 text-center text-white">Copyright &copy; opendevtools.org 2017 - <a href="/policy.php">Private
+                    Policy</a></p>
+            <form class="m-2 text-center" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+
+                <!-- Identify your business so that you can collect the payments. -->
+                <input type="hidden" name="business"
+                       value="opendevtools@gmail.com">
+
+                <!-- Specify a Donate button. -->
+                <input type="hidden" name="cmd" value="_donations">
+
+                <!-- Specify details about the contribution -->
+                <input type="hidden" name="item_name" value="Friends of Open Sources">
+                <input type="hidden" name="item_number" value="OpenDevTools Campaign">
+                <input type="hidden" name="currency_code" value="USD">
+
+                <!-- Display the payment button. -->
+                <input type="image" name="submit"
+                       src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_donate_92x26.png"
+                       alt="Donate">
+                <img alt="" width="1" height="1"
+                     src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+
+            </form>
+        </div>
     </div>
     <!-- /.container -->
 </footer>
