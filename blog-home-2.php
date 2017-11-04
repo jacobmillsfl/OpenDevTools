856c609eaaf70415db5ec497e1bcf5df8c76e6d0
carlaoutput <carla.output@gmail.com>
2017-11-04 17:35:45 -0400

    First temp blog entry in progress

--- blog-home-2.php
+++ blog-home-2.php
@@ -28,21 +28,26 @@
             <div class="row">
                 <div class="col-lg-6">
                     <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
+                        <img class="img-fluid rounded" src="/images/blog1.jpg" alt="">
                     </a>
                 </div>
                 <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                        atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                        voluptate voluptatibus possimus, veniam magni quis!</p>
+                    <h2 class="card-title">El éxito del open source</h2>
+                    <p class="card-text">
+                        El modelo open source podría y debería ser aplicado en todo tipo de investigación y desarrollo,
+                        no sólo en el de software. Si el esfuerzo científico adoptase un modelo abierto se ahorrarían costos y esfuerzos en procesos redundantes,
+                        y se podría acelerar la vía mediante la cual se obtienen resultados.
+
+
+
+                    </p>
                     <a href="#" class="btn btn-primary">Read More &rarr;</a>
                 </div>
             </div>
         </div>
         <div class="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
+            Posted on Noviembre 4, 2017 by
+            <a href="#">Carla Pasor</a>
         </div>
     </div>
 
@@ -52,71 +57,41 @@
             <div class="row">
                 <div class="col-lg-6">
                     <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
+                        <img class="img-fluid rounded" src="https://images.tynker.com/blog/wp-content/uploads/hero-girl-coding.png" alt="">
                     </a>
                 </div>
                 <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                        atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                        voluptate voluptatibus possimus, veniam magni quis!</p>
+                    <h2 class="card-title">Four Reasons Why Kids Should Learn to Program</h2>
+                    <p class="card-text">
+
+                        In recent years, the importance of programming has become a subject of increasing international
+                        awareness, moving from the narrow domain of the “geek” to the broader world, including the K-12
+                        education space. Earlier this year, President Obama endorsed mandatory computer programming
+                        education in schools. And recently,
+                        a viral video from code.org encouraged students to learn to code. For those of us in Silicon Valley,
+                        it is especially clear that the ability to code is taking on an unprecedented level of importance.
+
+                        Programming hasn’t become this popular by accident. There is a growing understanding that knowing how
+                        to program is essential, especially for younger generations. Simply memorizing facts is less and less
+                        relevant in a world where Google can satisfy just about any question in a matter of milliseconds;
+                        it is technical skills that will enable children to succeed, and that set of skills must include programming.
+
+
+                        </p>
                     <a href="#" class="btn btn-primary">Read More &rarr;</a>
                 </div>
             </div>
         </div>
         <div class="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
+            Posted on Noviembre 4, 2017 by
+            <a href="#"> Tynker.com</a>
         </div>
     </div>
 
-    <!-- Blog Post -->
-    <div class="card mb-4">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-lg-6">
-                    <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                        atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                        voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
-        </div>
-    </div>
 
-    <!-- Blog Post -->
-    <div class="card mb-4">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-lg-6">
-                    <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                        atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                        voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
-        </div>
-    </div>
+
+
+
 
     <!-- Pagination -->
     <ul class="pagination justify-content-center mb-4">
