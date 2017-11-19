f92e76079167a559d0cf1ac7382cdf90d78a626f
Rob Smitha <robsmitha@outlook.com>
2017-11-19 18:30:59 -0500

    Adjust UI of create blog button. Load all blog categories for sidebar widget. Adjust for each loop to print html for easier code readability. Disable Pagination.

--- bloghome.php
+++ bloghome.php
@@ -2,41 +2,30 @@
 /* Blog home page */
 session_start();
 include_once("DAL/Blog.php");
+include_once("DAL/BlogCategory.php");
+include_once("DAL/User.php");
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <?php include "head.php" ?>
-
 <body>
-
 <?php include "header.php" ?>
-
 <!-- Page Content -->
 <div class="container">
-
     <!-- Page Heading/Breadcrumbs -->
     <h1 class="mt-4 mb-3">OpenDevTools
         <small>Blog Home</small>
     </h1>
-
     <?php
-        //if logged in, show button to create blog
-        //$status= SessionManager::getUserId();
-        //if($status==0)
-        //{
-        //    echo '<div class = "text - right">';
-        //        echo '<a href="create - blog" class ="btn btn - info btn - lg">Create Blog</a>';
-        //    echo '</div>';
-        // }
+    //if logged in, show button to create blog
+    //$status= SessionManager::getUserId();
+    //if($status==0)
+    //{
+    //    echo '<div class = "text - right">';
+    //        echo '<a href="create - blog" class ="btn btn - info btn - lg">Create Blog</a>';
+    //    echo '</div>';
+    // }
     ?>
-
-    <div class = "text-right">
-        <a href="create-blog" class ="btn btn-info btn-lg">Create Blog</a>
-    </div>
-
     <ol class="breadcrumb">
         <li class="breadcrumb-item">
             <a href="index">Home</a>
@@ -44,70 +33,44 @@ include_once("DAL/Blog.php");
 
         <li class="breadcrumb-item active"> Blog</li>
     </ol>
-
-
-
     <div class="row">
-
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-
             <!-- uses the loadall() method from Blog.php to dynamically load blog images, titles, contents, dates, and user IDs -->
-
-        <?php
-        $blogList=Blog::loadall();
-        foreach ($blogList as $blogItem){
-            echo "<div class=\"card mb-4\">";
-            echo "<img class =\"card-img-top blog-home-img\" src=\"" .$blogItem->getImgUrl() ."\" alt=\"Blog Image\">";
-            echo "<div class=\"card-body\">";
-            echo "<h2 class=\"card-title\">" . $blogItem->getTitle() . "</h2>";
-            echo "<p class=\"card-text\">" . $blogItem->getContent() . "</p>";
-            //echo "<a href=\"#\" class=\"btn btn-primary\">" Read More "</a>"; //fix this
-            echo"</div>";
-            echo "<div class=\"card-footer text muted\">"  . $blogItem->getCreateDate() . "</div>";
-            echo "<a href=\"#\">".$blogItem-> getUserId(). "</a>";
-            echo "</div>";
-        }
-        ?>
-
-
-            <!-- Blog Post -->
-<!--            <div class="card mb-4">
-                <img class="card-img-top blog-home-img" src="/images/opensource.Carla.png" alt="Card image cap">
-                <div class="card-body">
-                    <h2 class="card-title">The success of Open Source</h2>
-                    <p class="card-text">A fundamental principle of open-source development is that the source code
-                        should be freely available. The source code of the program can be freely downloaded, used,
-                        modified and redistributed by anyone. The open source model could and should be applied in
-                        all types of research and development, not only in software.</p>
-                    <a href="/blogsample2" class="btn btn-primary">Read More &rarr;</a>
+            <?php
+            $blogList=Blog::loadall();
+            foreach ($blogList as $blogItem){
+                ?>
+                <div class="card mb-4">
+                    <img class="card-img-top blog-home-img" src="<?php echo $blogItem->getImgUrl(); ?>" alt="Card image cap">
+                    <div class="card-body">
+                        <h2 class="card-title"><?php echo $blogItem->getTitle(); ?></h2>
+                        <p class="card-text"><?php echo $blogItem->getContent(); ?></p>
+                        <!--<a href="/blogsample2" class="btn btn-primary">Read More &rarr;</a>-->
+                    </div>
+                    <div class="card-footer text-muted">
+                        Posted on <?php echo $blogItem->getCreateDate(); ?> by
+                        <a href="#">
+                            <?php
+                            $user = new User();
+                            $user->load($blogItem->getUserId());
+                            echo $user->getUsername();
+                            ?>
+                        </a>
+                    </div>
                 </div>
-                <div class="card-footer text-muted">
-                    Posted on Noviembre 4, 2017 by
-                    <a href="#"> Carla Pastor</a>
-                </div>
-            </div>-->
-
-
-            <!-- Blog Post -->
-<!--            <div class="card mb-4">
-                <img class="card-img-top blog-home-img" src="/images/go_lang_mascot_by_kirael_art-cpp15d.gif" alt="Blog Image">
-                <div class="card-body">
-                    <h2 class="card-title">Go, el mejor lenguaje de programación de código abierto creado por Google</h2>
-                    <p class="card-text">Go or más conocido como Golang ha ganado mucha fuerza, grandes empresas confían en golang, entre
-                        ellas; Facebook, MongoDB, Mozilla, Netflix, Comcast, Twitter, Shutterfly, y  Dropbox.
-                        Check la lista de compañías de todo el mundo que actualmente usan golang en el siguiente link
-                        <a href="https://github.com/golang/go/wiki/GoUsers" ><span style="color:#1e90ff">
-                                https://github.com/golang/go/wiki/GoUsers</span></a></p>
-                    <a href="/blogsample1" class="btn btn-primary">Read More &rarr;</a>
-                </div>
-                <div class="card-footer text-muted">
-                    Posted on Noviembre 5, 2017 by
-                    <a href="#"> Carla Pastor</a>
-                </div>
-            </div>-->
-
-                       <!-- Pagination -->
+                <?php
+            }
+            ?>
+            <ul class="pagination justify-content-center mb-4">
+                <li class="page-item disabled">
+                    <a class="page-link" href="#">&larr; Older</a>
+                </li>
+                <li class="page-item disabled">
+                    <a class="page-link" href="#">Newer &rarr;</a>
+                </li>
+            </ul>
+            <!-- Pagination Example
             <ul class="pagination justify-content-center mb-4">
                 <li class="page-item">
                     <a class="page-link" href="#">&larr; Older</a>
@@ -115,13 +78,16 @@ include_once("DAL/Blog.php");
                 <li class="page-item disabled">
                     <a class="page-link" href="#">Newer &rarr;</a>
                 </li>
-            </ul>
+            </ul> -->
 
         </div>
 
         <!-- Sidebar Widgets Column -->
         <div class="col-md-4">
-
+            <div class ="text-center">
+                <a href="create-blog" class ="btn btn-primary btn-lg btn-block"><i class=""></i> Create Blog</a>
+            </div>
+            <br>
             <!-- Search Widget -->
             <div class="card mb-4">
                 <h5 class="card-header">Search</h5>
@@ -140,49 +106,26 @@ include_once("DAL/Blog.php");
                 <h5 class="card-header">Categories</h5>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <a href="#">Web Design</a>
-                                </li>
-                                <li>
-                                    <a href="#">HTML</a>
-                                </li>
-                                <li>
-                                    <a href="#">Freebies</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <a href="#">JavaScript</a>
-                                </li>
-                                <li>
-                                    <a href="#">CSS</a>
-                                </li>
-                                <li>
-                                    <a href="#">Tutorials</a>
-                                </li>
-                            </ul>
-                        </div>
+                        <?php
+                        $BlogCategoryList = BlogCategory::loadall();
+                        foreach ($BlogCategoryList as $blogcategory){
+                            ?>
+                            <div class="col-lg-6">
+                                <a href="#"><?php echo $blogcategory->getName() ?></a>
+                            </div>
+                            <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
 
-            <!-- Side Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Side Widget</h5>
-                <div class="card-body">
-                    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-                </div>
-            </div>
 
         </div>
 
     </div>
 
-    </div><!-- /.row -->
+</div><!-- /.row -->
 </div><!-- /container -->
 
 <!-- Footer -->
