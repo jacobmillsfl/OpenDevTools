b8c35bc52c04c11ee6f64166c6998730d9cdd863
H0r53 <jacobmillsfl@gmail.com>
2017-11-20 19:49:27 -0500

    Added new images to the site banner and script to update site banner

---
+++ SQL/Data/INSERT_SiteBanner2.sql
@@ -0,0 +1,18 @@
+USE opendevtools;
+
+DELETE FROM SiteBanner;
+
+INSERT INTO SiteBanner(title,message,imgUrl)
+VALUES ("Award Winning","The OpenDevTools project won multiple awards at the 2017 FSU Computer Science Expo","images/expoFSU.png");
+
+INSERT INTO SiteBanner(title,message,imgUrl)
+VALUES ("Team Oriented","Software and services provided by OpenDevTools are created to support development teams of all sizes","images/groupPic3.jpeg");
+
+INSERT INTO SiteBanner(title,message,imgUrl)
+VALUES ("Security Focused","Our technology utilizes the latest security standards to ensure confidentiality and integrity of your data","images/cyberSec.jpeg");
+
+INSERT INTO SiteBanner(title,message,imgUrl)
+VALUES ("Inovative Technologies","Our team is always finding new and exciting ways to deliver services to our users","images/groupPic1.jpeg");
+
+INSERT INTO SiteBanner(title,message,imgUrl)
+VALUES ("Rapid Development","Our goal is to assist development teams with producing deliverables in a timely manner","images/groupPic2.jpeg");
