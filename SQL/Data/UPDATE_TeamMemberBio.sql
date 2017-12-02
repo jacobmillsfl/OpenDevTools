07cb8e8688fd17a65fd2da464ba935348a188af0
H0r53 <jacobmillsfl@gmail.com>
2017-12-02 15:12:02 -0500

    Jacob Mills, Carla Pastor - created script to update the bio of each team member

---
+++ SQL/Data/UPDATE_TeamMemberBio.sql
@@ -0,0 +1,22 @@
+use opendevtools;
+
+
+UPDATE TeamMember
+SET bio = 'Jacob Mills is a network administrator, database administrator, application developer, and cyber security specialist with a demonstrated history of working in the information technology and services industry. In 2014 he received his Associate of Science degree in Networking Services Technology from Tallahassee Community College, and he is currently working towards completing the combined Master/Bachelor of Science degree in Computer Science from Florida State University.'
+WHERE name = 'Jacob Mills';
+
+UPDATE TeamMember
+SET bio = 'Carla has several years of experience with business development, law, health, safety & environment (HSE), and quality assurance. She received her Bachelor degree in law from the University of Zulia State, Venezuela, and a Master of Venezuelan Labor Law from University of Dr. Belloso Chacin, Venezuela. She is currently pursing a Bachelor degree in Computer Science from Florida State University.'
+WHERE name = 'Carla Pastor';
+
+UPDATE TeamMember
+SET bio = 'Rob Smitha is a web applications developer in Tallahassee, Florida. He has extensive experience with web, graphics, and responsive design. Rob is a key contributor to the user interface design of multiple large scale projects used by millions of customers for both commercial and recreational purposes. Rob is currently pursing his Bachelor degree in Computer Science from Florida State University.'
+WHERE name = 'Rob Smitha';
+
+UPDATE TeamMember
+SET bio = 'Annie has always been interested in problem solving and technology. Throughout her experience with working in the service and hospitality industry, she has developed a niche for customer service and plans to integrate her skills in computer programming to help businesses maximize their customer experience and company efficiency. She is currently pursing a Bachelor degree in Computer Science from Florida State University.'
+WHERE name = 'Annie Liu';
+
+UPDATE TeamMember
+SET bio = 'Carlito has experience with application development and quality assurance on large projects for government partners. His development and testing experience is combined with communication skills to help facilitate the development and integration of software in mainstream environments. Carlito is pursing a Bachelor degree in Computer Science from Florida State University.'
+WHERE name = 'Carlito Que';
